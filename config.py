0b745ecc1ad4f54edaac5e41f39e1d9ce5b1f3f8
vovacooper <vovacooper@vovacooper-ubuntu.(none)>
2014-05-28 20:04:41 +0300

    index

---
+++ config.py
@@ -0,0 +1,5 @@
+__author__ = 'vovacooper'
+
+
+DATABADE_IP = "127.0.0.1"
+DATABASE_PORT = 27017
--- web.py
+++ web.py
@@ -1,10 +1,14 @@
 from flask import Flask, request
+
+#Flast entry point
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World! {0}".format(request.remote_addr)
 
+
+#MAIN
 if __name__ == "__main__":
     app.run()
 
