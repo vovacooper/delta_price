dc92afb81b9766c14ca67bed161abf49d7b38ad8
vovacooper <vovacooper@vovacooper-ubuntu.(none)>
2014-05-28 23:41:10 +0300

    upload

--- web.py
+++ web.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+from flask import url_for, redirect
+from flask import render_template
 
 #Flast entry point
 app = Flask(__name__)
@@ -6,7 +8,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World! {0}".format(request.remote_addr)
-
 
 #MAIN
 if __name__ == "__main__":
