b8e324ff4bcebabf70ba9829f083d196bafa147c
vovacooper <vovacooper@vovacooper-ubuntu.(none)>
2014-05-30 11:37:25 +0300

    static landing page and under_construction template

--- web.py
+++ web.py
@@ -7,7 +7,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! {0}".format(request.remote_addr)
+    return render_template('under_construction.html')
+   #return "Hello World! {0}".format(request.remote_addr)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello1(name=None):
+    return render_template('name_template.html', name=name)
 
 #MAIN
 if __name__ == "__main__":
